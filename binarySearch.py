43b5c57ebb762f8b32e76ce8b47419f4701c845b
MrWilllian <willian.dev10@gmail.com>
2020-03-31 10:51:54 -0300

    refactor: add recursive Binary Search algorithm

---
+++ binarySearch.py
@@ -0,0 +1,23 @@
+# Returns index of x in array if present
+def binarySearch(array, l, r, x):
+
+  # check base case
+  if r >= l:
+
+    middle: l + (r - l) // 2 #Get the middle index
+
+    # If element is present at the middle returns itself
+    if array[middle] == x:
+      return middle
+
+    # If element is smaller than middle, then it can only be present in left subarray
+    elif array[middle] > x:
+      return binarySearch(array, l, middle-1, x)
+
+    # Else the element can only be present in right subarray
+    else:
+      return binarySearch(array, middle+1, l, x)
+
+  else:
+    # Element is not present in the array
+    return -1
